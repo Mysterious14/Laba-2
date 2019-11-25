9d6a088e50460003ffea6b3b0c9cb6708c326b4f
Mysterious14 <57680026+Mysterious14@users.noreply.github.com>
2019-11-25 16:08:34 +0200

    Update Лаба 2.py

--- Лаба 2.py
+++ Лаба 2.py
@@ -1,17 +1,22 @@
-from collections import Counter
-s= input("Enter your text:")
-choice= input('Enter your choice(1,2):')
-if choice== "1" :
-    d={}
-    for i in s :
-         if i.isalpha():
-             d[i] = d.get(i,0)+1
+kal= '''!()-[]{};:'"\,<>./?@#$%^&*_~1234567890'''
+s = input("Enter your text:")
+choice = input('Enter your choice(1,2):')
+if choice == "1":
+    d = {}
+    for i in s:
+        if i.isalpha():
+            d[i] = d.get(i, 0) + 1
 
-    for i in sorted(d):
-        print(i,"=",d[i])
-elif choice=="2":
-    n = sorted(s.split())
-    print(n)
- 
+    for i in d:
+        print(i, "=", d[i])
+elif choice == "2":
+    no_s=""
+    for letter in s:
+        if letter not in kal:
+            no_s = no_s+ letter
+    no_s=no_s.split()
+    no_s.sort()
+    print("Sorted:")
+    for word in no_s:
+        print(word)
 else:
-    print("Wrong choice")
